d4cd563ad5fd0fc3b62a918196787bfdc22d27a9
Kendrick Jean <60457783+Kendrick-dot@users.noreply.github.com>
2020-05-18 15:07:55 -0400

    2

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-const inquirer =require("inquirer");
+const inquirer = require("inquirer");
 const fs = require('fs');
 const path = require('path');
-async function main(){
+async function main() {
     console.log(`starting`);
     const userResponse = await inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is your GitHub user name?",
-            name: "username"
-        },
-        {
-            type: "input",
-            message: "What is your Project Tittle?",
-            name: "projectTittle"
-        },
-        {
-            type: "input",
-            message: "Provide detail description",
-            name: "projectDescription"
-        },
-        {
-            type: "input",
-            message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-            name: "installationProcess"
-        },
-        {
-            type: "input",
-            message: "Provide instructions for use.",
-            name: "instruction"
-        },
-        {
-            type: "input",
-            message: "Provide instructions examples for use.",
-            name: "instructionExample"
-        },
-        {
-            type: "input",
-            message: "provide License name ",
-            name: "licenseName"
-        },
-        {
-            type: "input",
-            message: "provide License url ",
-            name: "licenseUrl"
-        },
-        {
-            type: "input",
-            message: "please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
-            name: "contributorsGitUserName"
-        },
-        {
-            type: "input",
-            message: "Provide examples on how to run tests.",
-            name: "tests"
-        }
+        .prompt([
+            {
+                type: "input",
+                message: "What is your GitHub user name?",
+                name: "username"
+            },
+            {
+                type: "input",
+                message: "What is your Project Title?",
+                name: "projectTitle"
+            },
+            {
+                type: "input",
+                message: "Provide description",
+                name: "projectDescription"
+            },
+            {
+                type: "input",
+                message: "Table of Contents",
+                name: "Contents"
+
+            },
+            {
+                type: "input",
+                message: "provide License name ",
+                name: "licenseName"
+            },
+            {
+                type: "input",
+                message: "please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
+                name: "contributorsGitUserName"
+            },
+            {
+                type: "input",
+                message: "Provide examples on how to run tests.",
+                name: "tests"
+            }
         ]);
+
+    console.log(`starting`);
+    console.log(userResponse);
+    const gitUsername = userResponse.username;
+    const projectTitle = userResponse.projectTitle;
+    const projectDescription = userResponse.projectDescription;
+    const licenseName = userResponse.licenseName;
+    const contributorUserNames = userResponse.contributorsGitUserName;
+    const tests = userResponse.tests;
+    const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
+    const gitData = gitResponse.data;
+    const gitName = gitData.login;
+    const gitEmail = gitData.email;
+    const gitUrl = gitData.html_url;
+    const gitProfileImage = gitData.avatar_url;
+    // contributor
+    const contributorUserNamesArray = contributorUserNames.split(",");
+    console.log(contributorUserNamesArray);
+    // const  = listOfContributorsUserNames.
+    // contributorsGitUserName
+    var resultContributor;
+    for (i = 0; i < contributorUserNamesArray.length; i++) {
+        var contributorsGitUserName = contributorUserNamesArray[i]
+        const gitResponse2 = await axios.get(`https://api.github.com/users/${contributorsGitUserName}`);
+        var gitContribuProfileImage = gitResponse2.data.avatar_url;
+        var gitContribuUrl = gitResponse2.data.html_url;
+        var gitContribuEmail = gitResponse2.data.email;
