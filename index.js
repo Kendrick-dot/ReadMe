eabce4a863531679cfcbdcf1e80e619c637628dd
Kendrick Jean <60457783+Kendrick-dot@users.noreply.github.com>
2020-07-14 21:47:26 -0400

    change 3

--- index.js
+++ index.js
@@ -1,71 +1,83 @@
+const fs = require("fs");
+
 const inquirer = require("inquirer");
-const fs = require('fs');
-const path = require('path');
-async function main() {
-    console.log(`starting`);
-    const userResponse = await inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is your GitHub user name?",
-                name: "username"
-            },
-            {
-                type: "input",
-                message: "What is your Project Title?",
-                name: "projectTitle"
-            },
-            {
-                type: "input",
-                message: "Provide description",
-                name: "projectDescription"
-            },
-            {
-                type: "input",
-                message: "Table of Contents",
-                name: "Contents"
 
-            },
-            {
-                type: "input",
-                message: "provide License name ",
-                name: "licenseName"
-            },
-            {
-                type: "input",
-                message: "please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
-                name: "contributorsGitUserName"
-            },
-            {
-                type: "input",
-                message: "Provide examples on how to run tests.",
-                name: "tests"
+const path = require("path");
+
+
+// the array of inquiries
+const questions = [
+
+    {
+        type: "input",
+        message: "Enter username for Github",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Enter title of project",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Describe the project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "List all installations and dependencies",
+        name: "installs",
+    },
+    {
+        type: "input",
+        message: "Enter instructions on how to use project",
+        name: "instructions",
+    },
+    {
+        type: "input",
+        message: "List the contributors of project",
+        name: "contributors",
+    }
+]
+console.clear();
+inquirer
+    .prompt(questions).then(response => {
+
+        fs.appendFileSync("README.md", ("#" + response.github) + '\n', function (err) {
+            if (err) {
+                return console.log(err);
             }
-        ]);
+            console.log("Success");
+        });
+        fs.appendFileSync("README.md", ("#" + response.title) + '\n', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success");
+        });
+        fs.appendFileSync("README.md", ("## Description" + '\n' + response.description) + '\n', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success");
+        });
+        fs.appendFileSync("README.md", ("## Installation" + response.installs) + '\n', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success");
+        });
+        fs.appendFileSync("README.md", ("## Instructions" + response.instructions) + '\n', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success");
+        });
+        fs.appendFileSync("README.md", ("## Contributors" + response.contributors) + '\n', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success");
+        });
+    });
 
-    console.log(`starting`);
-    console.log(userResponse);
-    const gitUsername = userResponse.username;
-    const projectTitle = userResponse.projectTitle;
-    const projectDescription = userResponse.projectDescription;
-    const licenseName = userResponse.licenseName;
-    const contributorUserNames = userResponse.contributorsGitUserName;
-    const tests = userResponse.tests;
-    const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
-    const gitData = gitResponse.data;
-    const gitName = gitData.login;
-    const gitEmail = gitData.email;
-    const gitUrl = gitData.html_url;
-    const gitProfileImage = gitData.avatar_url;
-    // contributor
-    const contributorUserNamesArray = contributorUserNames.split(",");
-    console.log(contributorUserNamesArray);
-    // const  = listOfContributorsUserNames.
-    // contributorsGitUserName
-    var resultContributor;
-    for (i = 0; i < contributorUserNamesArray.length; i++) {
-        var contributorsGitUserName = contributorUserNamesArray[i]
-        const gitResponse2 = await axios.get(`https://api.github.com/users/${contributorsGitUserName}`);
-        var gitContribuProfileImage = gitResponse2.data.avatar_url;
-        var gitContribuUrl = gitResponse2.data.html_url;
-        var gitContribuEmail = gitResponse2.data.email;
